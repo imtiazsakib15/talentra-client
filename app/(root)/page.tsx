00bcfad65b563a8e5bfd63e5bdf49564f821d4e8
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2025-08-23 22:19:33 +0600

    feat(meta): add homepage metadata

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,9 @@
 import Features from "@/components/home/Features";
 import Hero from "@/components/home/Hero";
 import HowItWorks from "@/components/home/HowItWorks";
+import { homePageMetadata } from "@/constants/metadata.constant";
+
+export const metadata = homePageMetadata;
 
 export default function Home() {
   return (
---
+++ constants/metadata.constant.ts
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+
+export const homePageMetadata: Metadata = {
+  title: "Talentra | Connect Companies & Candidates",
+  description:
+    "Talentra is a job portal that connects talented professionals with top companies. Find your dream job or the perfect candidate today!",
+  keywords: [
+    "talentra",
+    "job platform",
+    "hire candidates",
+    "find jobs",
+    "interview scheduling",
+    "talent search",
+    "employee finder",
+  ],
+};
