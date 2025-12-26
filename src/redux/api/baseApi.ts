bf763ff19d8d9067274f4ded6b33013e6f9c6020
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2025-12-27 00:29:00 +0600

    feat(auth): logout on 401 and 403

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,18 +1,52 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  createApi,
+  fetchBaseQuery,
+  type BaseQueryFn,
+  type FetchArgs,
+  type FetchBaseQueryError,
+} from "@reduxjs/toolkit/query/react";
 import type { RootState } from "../store";
+import { logout } from "../features/auth/authSlice";
+
+const rawBaseQuery = fetchBaseQuery({
+  baseUrl: import.meta.env.VITE_API_BASE_URL,
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+export const baseQueryWith403Handling: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  const result = await rawBaseQuery(args, api, extraOptions);
+  console.log(result.error);
+  if (result.error?.status === 401 || result.error?.status === 403) {
+    api.dispatch(logout());
+  }
+
+  return result;
+};
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
-    prepareHeaders: (headers, { getState }) => {
-      const token = (getState() as RootState).auth.token;
+  baseQuery: baseQueryWith403Handling,
 
-      if (token) {
-        headers.set("authorization", `Bearer ${token}`);
-      }
-      return headers;
-    },
-  }),
+  // fetchBaseQuery({
+  //   baseUrl: "http://localhost:5000/api/v1",
+  //   prepareHeaders: (headers, { getState }) => {
+  //     const token = (getState() as RootState).auth.token;
+
+  //     if (token) {
+  //       headers.set("authorization", `Bearer ${token}`);
+  //     }
+  //     return headers;
+  //   },
+  // }),
   endpoints: () => ({}),
 });
